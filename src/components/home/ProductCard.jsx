620f1a414594ba3a1512bed134d04da074e6381b
utku99 <56312196+utku99@users.noreply.github.com>
2023-05-17 16:43:23 +0300

    l

--- src/components/home/ProductCard.jsx
+++ src/components/home/ProductCard.jsx
@@ -6,7 +6,7 @@ const ProductCard = ({item}) => {
   const navigate = useNavigate()
 
   return (
-    <div onClick={()=> navigate(`products/${item.id}`)} className='w-[300px] p-2 m-2 border rounded-md relative cursor-pointer'>
+    <div onClick={()=> navigate(`products/${item.id}`)} className='w-[290px] p-2 m-2 border rounded-md relative cursor-pointer'>
         <div className='text-md font-bold absolute top-0 right-0'>{item.price} TL</div>
       <img className='w-[200px] h-[200px] m-auto' src={item.image} alt="img" />
       <div className='text-center py-2'>{item.title}</div>
